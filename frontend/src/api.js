f4dd6567ca13c69efd8cd8fd4d279bb37af2ec0d
OskarGrabar <gros24ul@student.ju.se>
2025-05-06 19:21:07 +0200

    Test

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // backend Spring API
+  baseURL: 'https://seal-app-rqsih.ondigitalocean.app/', // backend Spring API
 });
 
 export default api;
